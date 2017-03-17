8bb0e7a008eb91be95f004807319673d6fbda158
Andrew Scheuermann <andrewscheuermann@gmail.com>
2017-03-16 19:48:20 -0700

    add remaining two ReelPanels

--- src/Reel/component.js
+++ src/Reel/component.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './style.css';
 import ReelPanel from './ReelPanel/component';
-import { reelOneElements } from '../utils';
+import { reelOneElements, reelTwoElements, reelThreeElements } from '../utils';
 
 class Reel extends Component {
   render() {
     const { spinResults } = this.props;
-    const [ reelOneTarget ] = spinResults;
+    const [ reelOneTarget, reelTwoTarget, reelThreeTarget ] = spinResults;
     return (
       <div className="Reel">
         <ReelPanel targetElement={reelOneTarget} reelElements={reelOneElements} />  
+        <ReelPanel targetElement={reelTwoTarget} reelElements={reelTwoElements} />  
+        <ReelPanel targetElement={reelThreeTarget} reelElements={reelThreeElements} />  
       </div>
     )
   }
